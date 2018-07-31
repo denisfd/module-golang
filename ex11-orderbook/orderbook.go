30b77ce3490557a9a4f2fbdbb7e04c90e385e7fc
Denis Fedorchenko <dfedorchenko@heliostech.fr>
2018-07-31 17:07:19 +0300

    Perfomance increased (from <0.02 orders/sec to >7000000 orders/sec)

--- ex11-orderbook/orderbook.go
+++ ex11-orderbook/orderbook.go
@@ -1,9 +1,5 @@
 package orderbook
 
-import (
-//	"fmt"
-)
-
 type Orderbook struct {
 	LastID int
 	Bids   []*Order
@@ -20,62 +16,45 @@ func New() *Orderbook {
 }
 
 func (orderbook *Orderbook) Match(order *Order) ([]*Trade, *Order) {
-	//fmt.Printf("Order: %+v\n", *order)
-	//fmt.Printf("State:\n")
-	//for num, bid := range orderbook.Bids {
-	//fmt.Printf(" #%d: %+v\n", num, *bid)
-	//}
-	//for num, ask := range orderbook.Asks {
-	//fmt.Printf(" #%d: %+v\n", num, ask)
-	//}
-
-	var (
-		trades []*Trade
-		ord    *Order
-	)
-	switch {
-
-	case order.Side == SideAsk && order.Kind == KindMarket:
-		trades, ord = orderbook.MarketAsk(order)
-
-	case order.Side == SideBid && order.Kind == KindMarket:
-		trades, ord = orderbook.MarketBid(order)
-
-	case order.Side == SideAsk && order.Kind == KindLimit:
-		trades, ord = orderbook.LimitAsk(order)
-
-	case order.Side == SideBid && order.Kind == KindLimit:
-		trades, ord = orderbook.LimitBid(order)
+	switch order.Side {
+	case SideAsk:
+		return orderbook.LimitAsk(order)
+	case SideBid:
+		return orderbook.LimitBid(order)
 	}
 
-	//fmt.Printf("Trades generated: %+v\n", trades)
-	//for num, trade := range trades {
-	//fmt.Printf(" #%d: %+v\n", num, *trade)
-	//}
-	//fmt.Printf("\n")
-
-	return trades, ord
+	return nil, nil
 }
 
-func (ob *Orderbook) MarketBid(order *Order) ([]*Trade, *Order) {
-	return ob.LimitBid(order)
-}
+func (ob *Orderbook) Cancel(ID int) bool {
+	for i, order := range ob.Bids {
+		if order.ID == ID {
+			ob.Bids = append(ob.Bids[:i], ob.Bids[i+1:]...)
+			return true
+		}
+	}
 
-func (ob *Orderbook) MarketAsk(order *Order) ([]*Trade, *Order) {
-	return ob.LimitAsk(order)
+	for i, order := range ob.Asks {
+		if order.ID == ID {
+			ob.Asks = append(ob.Asks[:i], ob.Asks[i+1:]...)
+			return true
+		}
+	}
+
+	return false
 }
 
 func (ob *Orderbook) LimitAsk(order *Order) ([]*Trade, *Order) {
-	//	println("Limit Ask called")
 	trades := []*Trade{}
 
 	for i := 0; i < len(ob.Asks); i++ {
 		ask := ob.Asks[i]
-		if order.Price == 0 || ask.Price >= order.Price {
-			trade := &Trade{}
-			trade.Price = ask.Price
-			trade.Bid = order
-			trade.Ask = ask
+		if order.Price == 0 || order.Price <= ask.Price {
+			trade := &Trade{
+				Price: ask.Price,
+				Bid:   order,
+				Ask:   ask,
+			}
 
 			if ask.Volume > order.Volume {
 				trade.Volume = order.Volume
@@ -93,6 +72,8 @@ func (ob *Orderbook) LimitAsk(order *Order) ([]*Trade, *Order) {
 			if order.Volume == 0 {
 				break
 			}
+		} else {
+			break
 		}
 	}
 
@@ -100,25 +81,23 @@ func (ob *Orderbook) LimitAsk(order *Order) ([]*Trade, *Order) {
 		if order.Price == 0 {
 			return trades, order
 		}
-		ob.Bids = append(ob.Bids, order)
-		ob.SortBids()
-		//fmt.Printf("Bid added: %+v\n", *order)
+		ob.AddBid(order)
 	}
 
 	return trades, nil
 }
 
 func (ob *Orderbook) LimitBid(order *Order) ([]*Trade, *Order) {
-	//	println("Limit Bid called")
 	trades := []*Trade{}
 
 	for i := 0; i < len(ob.Bids); i++ {
 		bid := ob.Bids[i]
 		if order.Price == 0 || bid.Price <= order.Price {
-			trade := &Trade{}
-			trade.Price = bid.Price
-			trade.Bid = bid
-			trade.Ask = order
+			trade := &Trade{
+				Price: bid.Price,
+				Bid:   bid,
+				Ask:   order,
+			}
 
 			if bid.Volume > order.Volume {
 				trade.Volume = order.Volume
@@ -136,6 +115,8 @@ func (ob *Orderbook) LimitBid(order *Order) ([]*Trade, *Order) {
 			if order.Volume == 0 {
 				break
 			}
+		} else {
+			break
 		}
 	}
 
@@ -143,30 +124,30 @@ func (ob *Orderbook) LimitBid(order *Order) ([]*Trade, *Order) {
 		if order.Price == 0 {
 			return trades, order
 		}
-		//	fmt.Printf("Ask added: %+v\n", *order)
-		ob.Asks = append(ob.Asks, order)
-		ob.SortAsks()
+		ob.AddAsk(order)
 	}
 
 	return trades, nil
 }
 
-func (ob *Orderbook) SortBids() { //loswest first
-	for i := 0; i < len(ob.Bids); i++ {
-		for j := len(ob.Bids) - 1; j > i; j-- {
-			if ob.Bids[j-1].Price > ob.Bids[j].Price {
-				ob.Bids[j-1], ob.Bids[j] = ob.Bids[j], ob.Bids[j-1]
-			}
+func (ob *Orderbook) AddBid(bid *Order) {
+	ob.Bids = append(ob.Bids, bid)
+	for i := len(ob.Bids) - 1; i > 0; i-- {
+		if ob.Bids[i].Price < ob.Bids[i-1].Price {
+			ob.Bids[i], ob.Bids[i-1] = ob.Bids[i-1], ob.Bids[i]
+		} else {
+			break
 		}
 	}
 }
 
-func (ob *Orderbook) SortAsks() { //loswest first
-	for i := 0; i < len(ob.Asks); i++ {
-		for j := len(ob.Asks) - 1; j > i; j-- {
-			if ob.Asks[j-1].Price < ob.Asks[j].Price {
-				ob.Asks[j-1], ob.Asks[j] = ob.Asks[j], ob.Asks[j-1]
-			}
+func (ob *Orderbook) AddAsk(ask *Order) {
+	ob.Asks = append(ob.Asks, ask)
+	for i := len(ob.Asks) - 1; i > 0; i-- {
+		if ob.Asks[i].Price > ob.Asks[i-1].Price {
+			ob.Asks[i], ob.Asks[i-1] = ob.Asks[i-1], ob.Asks[i]
+		} else {
+			break
 		}
 	}
 }
